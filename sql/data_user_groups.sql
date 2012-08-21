c30fd7c6bf9ac03c8e6d785d284596af45d364c4
Marc Chung <mchung@gmail.com>
2012-08-20 22:25:24 -0700

    Fix typo

--- sql/data_user_groups.sql
+++ sql/data_user_groups.sql
@@ -19,4 +19,4 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 INSERT INTO `user_group` VALUES(1, 'admin', 'Site admin/super users', 1, '2011-07-12 15:07:17');
 INSERT INTO `user_group` VALUES(2, 'moderator', 'Site moderators', 1, '2011-07-12 15:07:17');
-INSERT INTO `user_group` VALUES(3, 'leader', 'Reponse leaders/"endorsers"', 1, '2011-07-12 15:07:17');
+INSERT INTO `user_group` VALUES(3, 'leader', 'Response leaders/"endorsers"', 1, '2011-07-12 15:07:17');
